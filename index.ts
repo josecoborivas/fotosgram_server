11b4e40e56f21a0b1329b9d88a1d52a44bf531b0
josecoborivas <josecoborivas@gmail.com>
2020-06-29 15:29:52 -0300

    instancia express

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log('Hola Jose Luis Cobo Rivas!')
+import Server from './classes/server';
+
+const server = new Server();
+
+//Levantar server
+server.start(() => {
+    console.log(`Servidor corriento en puerto: ${server.port}`);
+})
