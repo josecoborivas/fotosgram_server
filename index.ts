50a39e2d5392dea2cd96462fec5a80b4f48822d3
josecoborivas <josecoborivas@gmail.com>
2020-06-29 16:04:38 -0300

    models

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import Server from './classes/server';
 import usuariosRoutes from './routes/usuarios.routes';
+import mongoose from 'mongoose';
 
 const server = new Server();
 
 //Rutas de mi App
 server.app.use('/user/', usuariosRoutes);
+
+//Conectar Base de Datos Mongo
+mongoose.connect('mongodb://localhost:27017/fotosgram', { useNewUrlParser: true, useCreateIndex: true }, err => {
+    if(err) throw err;
+
+    console.log('BASE DE DATOS ONLINE!');
+});
 
 //Levantar server
 server.start(() => {
