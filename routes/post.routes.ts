d7198a8826a39bb36834900b957dd7868424ecac
josecoborivas <josecoborivas@gmail.com>
2020-06-30 15:23:30 -0300

    obtener ports por pagina

--- routes/post.routes.ts
+++ routes/post.routes.ts
@@ -4,6 +4,32 @@ import { Post } from '../models/post.model';
 
 const postRouter = Router();
 
+
+
+//Obtener Post por pagina
+postRouter.get('/', [verificarToken], async (req: any, res: Response)=> {
+
+    let pagina = Number(req.query.pagina) || 1;
+    let skip = pagina -1;
+    skip = skip * 10;
+
+    const posts = await Post.find()
+                                    .sort({'_id': -1})
+                                    .limit(10)
+                                    .skip(skip)
+                                    .populate('usuario', '-password')
+                                    .exec();
+
+    res.json({
+        ok: true,
+        pagina,
+        posts
+    });
+});
+
+
+
+//Crear Post
 postRouter.post('/', [verificarToken], (req: any, res: Response)=> {
 
     const body = req.body;
